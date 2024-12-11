00b38465e9fd68a63ff07eafc19034173652bf54
Gustavo Arruda <151965666+gustavoarruda1@users.noreply.github.com>
2024-12-11 10:34:05 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const menuMobile = document.querySelector('.menu');
 const icon = document.querySelector('.icon');
 const menuButton = document.getElementById('menu-mobile');
+const menuLinks = document.querySelectorAll('.menu a');
 
 // Função que abre e fecha o menu
 function menuView() {
@@ -18,8 +19,15 @@ document.addEventListener('click', function (event) {
     if (menuMobile.classList.contains('open') &&
         !menuMobile.contains(event.target) &&
         !menuButton.contains(event.target)) {
-
         menuMobile.classList.remove('open');
         icon.src = "./img/icon-menu.svg";
     }
+});
+
+// Fecha o menu ao clicar em qualquer link dentro dele
+menuLinks.forEach(link => {
+    link.addEventListener('click', function () {
+        menuMobile.classList.remove('open');
+        icon.src = "./img/icon-menu.svg";
+    });
 });
